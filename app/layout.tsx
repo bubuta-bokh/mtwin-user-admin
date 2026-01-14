9e12d9f1e95b738f0eef003ab8644d119e497fc1
bubuta-bokh <romanbr70@gmail.com>
2026-01-14 12:48:39 +0100

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/shared/navbar";
+import { AuthInitializer } from "./auth-initializer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +28,10 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning={true}
       >
+        <AuthInitializer />
+        <Navbar />
         {children}
       </body>
     </html>
